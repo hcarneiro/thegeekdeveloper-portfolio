d0c5a4e3bdab6bcead4e7d26ace7d1a3ae0feb05
hcarneiro <hcarneiro@weboo.biz>
2019-08-04 15:28:05 +0100

    Use <a> on logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="logo-wrapper" :class="{ 'no-flex': inMenu }">
-    <nuxt-link class="logo" tag="div" :to="'/'" />
-    <div v-if="!inMenu" class="logo-text">
-      The Geek Developer
-    </div>
+    <nuxt-link class="logo-holder" :to="'/'">
+      <div class="logo" />
+      <div v-if="!inMenu" class="logo-text">
+        The Geek Developer
+      </div>
+    </nuxt-link>
   </div>
 </template>
 
